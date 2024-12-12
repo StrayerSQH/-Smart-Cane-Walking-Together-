7d0ae8d2a19377db923398d3eca54814e9f3b7a7
Qianhao Sun <154659308+StrayerSQH@users.noreply.github.com>
2024-12-12 17:10:14 +0800

    Update BluetoothMasterDevice.ino

--- 硬件（Hardware)/Version_0_With_BlueTooth/BluetoothMasterDevice.ino
+++ 硬件（Hardware)/Version_0_With_BlueTooth/BluetoothMasterDevice.ino
@@ -1,1 +1,17 @@
+#include <SoftwareSerial.h>
 
+#define RX 3
+#define TX 4
+
+SoftwareSerial BT(RX, TX); 
+
+void setup() {
+  Serial.begin(38400);
+  BT.begin(38400);
+  pinMode(RX, INPUT);
+  pinMode(TX, OUTPUT);
+}
+
+void loop() {
+  BT.write('C');
+}
